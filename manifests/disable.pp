01c6f36418dacd81075c9f5885d3c2643767088c
Simon Josi <simon.josi@atizo.com>
2010-11-12 19:24:26 +0100

    add new copyright notice; switch to two space indentation

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -1,12 +1,5 @@
-#######################################
-# acpid module - disable.pp
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-#######################################
-
-# remove acpid
-class acpid::disable {
-    package{'acpid':
-        ensure => absent,
-    }
+class acpid::disable inherits acpid {
+  Service['acpid']{
+    ensure => stopped,
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,9 @@
 # acpid module
 #
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -11,20 +12,14 @@
 # the Free Software Foundation.
 #
 
-# modules_dir { "acpid": }
-
 class acpid {
-    include acpid::base 
-}
-
-class acpid::base {
-    package{acpid:
-        ensure => present,
-    }
-    service{acpid:
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package[acpid],
-    }
+  package{'acpid':
+    ensure => present,
+  }
+  service{'acpid':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package['acpid'],
+  }
 }
